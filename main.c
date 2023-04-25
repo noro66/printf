764eac636ac60dae797a31c04d248465f339051e
Nouamane <norou6@gmail.com>
2023-04-25 13:18:53 -0700

    seccess

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	 _printf("Let's try to printf a simple sentence.\n");
+	 printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+
+	 return(0);
+}
