839a1f887a3015fa9ccbaded7dda19e98105ad09
Nouamane <norou6@gmail.com>
2023-04-25 16:22:41 -0700

    done

--- check_func.c
+++ check_func.c
@@ -14,22 +14,30 @@ int check_func(char id, va_list args)
 {
 	int i = 0;
 
+	/* the fuction printf takes you her if the format is not a regulare text*/
+	 /* to print the arlist , it depens on the type*/
+	 /* of argument that spesefied by the user.*/
+	/* the user spesesier the type by print '%'*/
+	/*and a character of a sign.*/
+
 	switch (id)
 	{
-	case 'c':
+	case 'c':/*if that character is a 'c' the function take you another function*/
+		/* theat types the arlist in character type.*/
 		i += prnt_ch(args);
 		break;
-	case 's':
+	case 's':	/* if 's' to ... strintgs type.*/
 		i += prnt_str(args);
 		break;
-	case '%':
+	case '%':	/*just type the same '%',*/
 		i += _putchar('%');
 		break;
 	case 'd':
-	case 'i':
+	case 'i':/* if ... 'd || i' .. integers type */
 		i += prnt_int(args);
 		break;
-	default:
+	default:/* if the user types onther character it prints '%',*/
+	       /*and that character.*/
 		i += _putchar('%');
 		i += _putchar(id);
 		break;
--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: void .
+ * Return: int .
  */
 
-void main(void)
+int  main(void)
 {
-	 _printf("Let's try to printf a simple sentence.\n");
-	 printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
+	int len = 0;
+	int len1 = 0;
 
+	len =  _printf("Let's try to printf a simple sentence.\n");
+	len1 = _printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	_printf("Length:[%d, %i]\n", len1, len);
+	printf("Length:[%d, %i]\n", len, len1);
+	_putchar('\n');
+	return (0);
 }
