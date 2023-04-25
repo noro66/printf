3f08c757fbdc7f19ed69b62be4e3fd8a09c12f01
Nouamane <norou6@gmail.com>
2023-04-25 12:05:26 -0700

    ok

--- check_func.c
+++ check_func.c
@@ -6,17 +6,25 @@
 int (*check_func(const char *p))(va_list)
 {
 	int i;
+	int j;
+	fnc_t array[]={
+		{"c",prnt_ch(args)},
+		{"s",prnt_str(args)},
+		};
 
-	fnc_t chekers[4]={
-		{"c",prnt_ch},
-		{"s",prnt_str},
-		{"i",prnt_int},
-		{"d",prnt_int},
-		{NULL,NULL},
-				};
-	for (i = 0;chekers[i].t != NULL ;i++)
+	for (i = 0; array[i].t != NULL ;i++)
 	{
 		if (*(chekers[i].t) == *p)
-			return (chekers[i].f);
+		{
+			j += (array[i].f);
+			break;
+		}
+		else 
+		{
+		j += _putchar('%');
+		j += _putchar(id);
+		break;
+		}
 	}
+	return (j);
 }
