f0b38b9581c8be5fb1773fbd062e25a0ae58abc3
Mastro37 <bouklilmohamed@gmail.com>
2023-04-25 14:49:29 -0700

    done

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: void .
+ */
 
-int main(void)
+void main(void)
 {
 	 _printf("Let's try to printf a simple sentence.\n");
 	 printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
+	_printf("String:[%s]\n", "I am a string !");
 
-	 return(0);
 }
